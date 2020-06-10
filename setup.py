a428849d692f5c6bda14868fb85dd153a4219fc5
Nebojša Jakovljević <nebojsa@nebjak.net>
2020-06-10 21:50:49 +0200

    additional settings

--- setup.py
+++ setup.py
@@ -18,8 +18,14 @@ setuptools.setup(
     include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    license="MIT License",
+    keywords="serbia zipcode postcode"
 )
